0d47017aa00ecb0c3e897adc10332403f4f516bd
James Andersen <james.andersen@universalmind.com>
2017-08-03 15:55:01 -0600

    Ensure PWD is script directory when training script runs

--- keras-deeplearning/train_model.sh
+++ keras-deeplearning/train_model.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -xe
 
+# Change working directory to script directory
+cd "${0%/*}"
+
 unzip -o ../LoanStats3d_securev1.csv.zip -d .
 python ../lending_club_clean.py `pwd`/lc-2015-loans.csv
 rm LoanStats3d_securev1.csv
