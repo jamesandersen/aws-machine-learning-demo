cac2ea41d4dc90fcaa2bed5a5a28d8deae6391f9
James Andersen <james.andersen@universalmind.com>
2017-08-03 14:54:28 -0600

    Updates to training script and template

--- keras-deeplearning/train_model.sh
+++ keras-deeplearning/train_model.sh
@@ -3,4 +3,11 @@
 unzip -o ../LoanStats3d_securev1.csv.zip -d .
 python ../lending_club_clean.py `pwd`/lc-2015-loans.csv
 rm LoanStats3d_securev1.csv
-jupyter nbconvert --ExecutePreprocessor.timeout=600 --execute lending-club-loan-grades.ipynb
+
+# Lengthy timeout here should cover the training time on p2.xlarge
+jupyter nbconvert --ExecutePreprocessor.timeout=2400 --execute lending-club-loan-grades.ipynb
+
+# TODO:
+# Send trained model and HTML output to S3
+
+# Delete the stack that created this instance
