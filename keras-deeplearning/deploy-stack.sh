1919c9d8e4cc2755722a69a15ef455f25df7487e
James Andersen <james.andersen@universalmind.com>
2017-08-03 12:37:41 -0600

    Work in progress on keras cloud formation stack

--- keras-deeplearning/deploy-stack.sh
+++ keras-deeplearning/deploy-stack.sh
@@ -2,16 +2,16 @@
 
 aws cloudformation validate-template --template-body file://train-model.template
 
-: <<'END'
+#: <<'END'
 aws cloudformation create-stack --stack-name deep-learning-spot-instance --template-body file://train-model.template \
-    --parameters ParameterKey=Environment,ParameterValue=test \
-        ParameterKey=InstanceType,ParameterValue=p2.xlarge \
+    --parameters ParameterKey=InstanceType,ParameterValue=p2.xlarge \
         ParameterKey=SpotBidPrice,ParameterValue=0.75 \
-        ParameterKey=KeyPairName,ParameterValue=OregonKeyPair \
+        ParameterKey=KeyPairName,ParameterValue=UMOregon \
         ParameterKey=GitRepo,ParameterValue=https://github.com/jamesandersen/aws-machine-learning-demo.git \
         ParameterKey=GitBranch,ParameterValue=aws-keras-deeplearning \
         ParameterKey=RunScript,ParameterValue=keras-deeplearning/train_model.sh \
-    --disable-rollback
-    --region us-west-2
+    --capabilities CAPABILITY_NAMED_IAM \
+    --disable-rollback \
+    --region us-west-2 \
     --profile um
-END
+#END
--- keras-deeplearning/train_model.sh
+++ keras-deeplearning/train_model.sh
@@ -3,3 +3,4 @@
 unzip -o ../LoanStats3d_securev1.csv.zip -d .
 python ../lending_club_clean.py `pwd`/lc-2015-loans.csv
 rm LoanStats3d_securev1.csv
+jupyter nbconvert --ExecutePreprocessor.timeout=600 --execute lending-club-loan-grades.ipynb
